d998c8310bb8ea255eb407e6d4265b3d8550e5fc
arslee <me@arslee.dev>
2022-02-23 14:40:34 +0900

    chore: Update example

--- example/example.dart
+++ example/example.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:revolt/revolt.dart';
 
 void main() async {
@@ -6,10 +8,29 @@ void main() async {
     botToken: '',
   );
 
-  client.ws.onRawEvent.stream.listen(print);
+  client.ws.onMessage.stream.where((m) => m.content is String)
+    ..where((m) => m.content.startsWith('a.ping')).listen(
+      (e) async {
+        await client.rest.sendMessage(
+          channelId: e.channel,
+          message: MessageBuilder.reply(
+            'Pong! :amogus:',
+            replies: [MessageReplyBuilder(e.id, mention: true)],
+          ),
+        );
+      },
+    )
+    ..where((m) => m.content.startsWith('a.random')).listen(
+      (e) async {
+        await client.rest.sendMessage(
+          channelId: e.channel,
+          message: MessageBuilder.reply(
+            Random().nextInt(1000).toString(),
+            replies: [MessageReplyBuilder(e.id, mention: true)],
+          ),
+        );
+      },
+    );
 
   await client.connect();
-
-  final user = await client.rest.fetchSelf();
-  print(user.username);
 }
