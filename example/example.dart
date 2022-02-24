b9cb01ff7ce8be279babe2a64f92b115bd479c8e
arslee <me@arslee.dev>
2022-02-24 13:32:54 +0900

    chore: Update example

--- example/example.dart
+++ example/example.dart
@@ -13,9 +13,9 @@ void main() async {
       (e) async {
         await client.rest.sendMessage(
           channelId: e.channel,
-          message: MessageBuilder.reply(
-            'Pong! :amogus:',
-            replies: [MessageReplyBuilder(e.id, mention: true)],
+          message: MessageBuilder(
+            content: 'Pong! :amogus:',
+            replies: [MessageReplyBuilder(e.id)],
           ),
         );
       },
@@ -24,9 +24,29 @@ void main() async {
       (e) async {
         await client.rest.sendMessage(
           channelId: e.channel,
-          message: MessageBuilder.reply(
-            Random().nextInt(1000).toString(),
-            replies: [MessageReplyBuilder(e.id, mention: true)],
+          message: MessageBuilder(
+            content: Random().nextInt(1000).toString(),
+            replies: [MessageReplyBuilder(e.id)],
+          ),
+        );
+      },
+    )
+    ..where((m) => m.content.startsWith('a.fullmessage')).listen(
+      (e) async {
+        await client.rest.sendMessage(
+          channelId: e.channel,
+          message: MessageBuilder(
+            content: 'Full message test :amogus:',
+            replies: [MessageReplyBuilder(e.id)],
+            embeds: [
+              TextEmbedBuilder(title: 'among us', description: ':amogus:'),
+            ],
+            masquerade: MasqueradeBuilder(
+              name: 'among_us_gamer_6969',
+              avatar: Uri.parse(
+                'http://www.rw-designer.com/icon-image/21508-256x256x32.png',
+              ),
+            ),
           ),
         );
       },
