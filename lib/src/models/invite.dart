a4cc47161bea92e8002d1e39698471d7747546f9
arslee <me@arslee.dev>
2022-03-17 08:27:57 +0900

    feat(models): Implement joined invite

--- lib/src/models/invite.dart
+++ lib/src/models/invite.dart
@@ -1,4 +1,6 @@
 import 'package:revolt/src/models/attachment.dart';
+import 'package:revolt/src/models/channel.dart';
+import 'package:revolt/src/models/server.dart';
 import 'package:revolt/src/models/ulid.dart';
 import 'package:revolt/src/utils/enum.dart';
 
@@ -105,5 +107,44 @@ class ServerInvite extends Invite {
             ? null
             : Attachment.fromJson(json['user_avatar']),
         memberCount = json['member_count'],
+        super.fromJson(json);
+}
+
+/// Type of joined invite
+class JoinedInviteType extends Enum<String> {
+  static const server = JoinedInviteType._create('Server');
+
+  JoinedInviteType.from(String value) : super(value);
+  const JoinedInviteType._create(String value) : super(value);
+}
+
+/// Minimal joined invite
+abstract class JoinedInvite {
+  /// Joined invite type
+  final JoinedInviteType type;
+
+  JoinedInvite({required this.type});
+
+  JoinedInvite.fromJson(Map<String, dynamic> json)
+      : type = JoinedInviteType.from('type');
+}
+
+/// Server joined invite
+class ServerJoinedInvite extends JoinedInvite {
+  /// Invited channel
+  final Channel channel;
+
+  /// Invited server
+  final Server server;
+
+  ServerJoinedInvite({
+    required JoinedInviteType type,
+    required this.channel,
+    required this.server,
+  }) : super(type: type);
+
+  ServerJoinedInvite.fromJson(Map<String, dynamic> json)
+      : channel = Channel.define(json['channel']),
+        server = Server.fromJson(json['server']),
         super.fromJson(json);
 }
